58612b22b73a1fe401f7ca2208f9f3818b340978
yuanR <yuanrui@supermap.com>
2017-09-22 16:33:36 +0800

    生成空间权重矩阵文件功能丰富，可与其他工作流功能连接 yuanR

--- WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/metaProcessImplements/spatialStatistics/ParameterPatternsParameter.java
+++ WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/metaProcessImplements/spatialStatistics/ParameterPatternsParameter.java
@@ -100,7 +100,6 @@ public class ParameterPatternsParameter extends ParameterCombine {
 					CoreProperties.getString("String_Open"), modelName, "OpenOne");
 		}
 		this.parameterFile.setModuleName(modelName);
-		this.parameterFile.setRequisite(true);
 
 		this.parameterTextFieldKNeighbors.setDescribe(ProcessProperties.getString("String_KNeighbors"));
 		this.parameterSelfWeightFieldComboBox.setDescribe(ProcessProperties.getString("String_SelfWeightField"));
