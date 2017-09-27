378be6a86d9f1df848da637c06d6870df8c26d0c
highsad <wuxiaobo@supermap.com>
2017-09-27 17:41:21 +0800

    优化注释。

--- Process/src/main/java/com/supermap/desktop/process/enums/RunningStatus.java
+++ Process/src/main/java/com/supermap/desktop/process/enums/RunningStatus.java
@@ -3,7 +3,7 @@ package com.supermap.desktop.process.enums;
 /**
  * Created by highsad on 2017/6/15.
  * 警告状态不合适，警告不是错误，不影响运行，也就是说警告状态和其他状态是可以共存的
- * 后续再行优化
+ * 后续再重新优化 Workflow 和 Process 的警告实现
  */
 public enum RunningStatus {
 	NORMAL(0), RUNNING(1), COMPLETED(2), EXCEPTION(3), CANCELLING(4), CANCELLED(5), READY(6), WARNING(7);
