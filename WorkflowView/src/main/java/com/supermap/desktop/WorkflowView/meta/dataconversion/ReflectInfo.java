0d028fff0edd59e016db18afa7862e8264417ac3
yuanR <yuanrui@supermap.com>
2017-09-28 14:24:01 +0800

    导入csv功能工作流支持导入点线面，面板实现 yuanR

--- WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/dataconversion/ReflectInfo.java
+++ WorkflowView/src/main/java/com/supermap/desktop/WorkflowView/meta/dataconversion/ReflectInfo.java
@@ -4,6 +4,7 @@ import com.supermap.desktop.process.parameter.interfaces.IParameter;
 
 /**
  * Created by xie on 2017/3/31.
+ * 工作流导入参数包装类，以名称和参数本身进行构建
  */
 public class ReflectInfo {
     public String methodName;
