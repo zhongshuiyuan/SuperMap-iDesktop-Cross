9ebbc472f44cd70b16d0d452b056ed7ac185641f
lixiaoyao <liwenfa@supermap.com>
2017-10-19 10:12:41 +0800

    上一页和下一页逻辑算法优化以及新建根组和新建分组代码初步实现——李文发

--- MapView/src/main/java/com/supermap/desktop/CtrlAction/LayerSetting/CtrlActionCreateRootLayerGroup.java
+++ MapView/src/main/java/com/supermap/desktop/CtrlAction/LayerSetting/CtrlActionCreateRootLayerGroup.java
@@ -1,10 +1,12 @@
 package com.supermap.desktop.CtrlAction.LayerSetting;
 
 import com.supermap.desktop.Application;
+import com.supermap.desktop.FormMap;
 import com.supermap.desktop.Interface.IBaseItem;
 import com.supermap.desktop.Interface.IForm;
 import com.supermap.desktop.implement.CtrlAction;
-import com.supermap.desktop.mapview.MapViewProperties;
+import com.supermap.desktop.ui.UICommonToolkit;
+import com.supermap.desktop.ui.controls.LayersTree;
 
 /**
  * Created by lixiaoyao on 2017/10/11.
@@ -13,12 +15,21 @@ public class CtrlActionCreateRootLayerGroup extends CtrlAction {
 
 	public CtrlActionCreateRootLayerGroup(IBaseItem caller, IForm formClass) {
 		super(caller, formClass);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void run() {
-		Application.getActiveApplication().getOutput().output(MapViewProperties.getString("String_CurrentFunctionDeveloping"));
+		IForm iForm=Application.getActiveApplication().getActiveForm();
+		if (iForm instanceof FormMap){
+			FormMap formMap=(FormMap)iForm;
+			String layerGroupName=formMap.getMapControl().getMap().getLayers().getAvailableCaption("LayerGroup");
+			formMap.getMapControl().getMap().getLayers().addGroup(layerGroupName);
+			LayersTree layersTree = UICommonToolkit.getLayersManager().getLayersTree();
+			int selectRow=layersTree.getRowCount()-1;
+			layersTree.setSelectionRow(selectRow);
+			layersTree.startEditingAtPath(layersTree.getPathForRow(selectRow));
+		}
+//		Application.getActiveApplication().getOutput().output(MapViewProperties.getString("String_CurrentFunctionDeveloping"));
 	}
 
 	@Override
